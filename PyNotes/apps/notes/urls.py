5b28880d1fa21b467cbf6b0e0f0c85c6c9b9bd6c
FoxyChmoxy <almasgaara@mail.ru>
2019-10-28 01:14:26 +0600

    Resolve: Added delete Category and Note

--- PyNotes/apps/notes/urls.py
+++ PyNotes/apps/notes/urls.py
@@ -7,6 +7,8 @@ urlpatterns = [
     path('<int:category_id>/notes/', views.categoryById, name='categoryById'),
     path('<int:category_id>/notes/<int:note_id>/<is_success>', views.noteById, name='noteById'),
     path('create/', views.createCategory, name='createCategory'),
+    path('<int:category_id>/delete/', views.deleteCategory, name='deleteCategory'),
     path('<int:category_id>/notes/create/', views.createNote, name='createNote'),
+    path('<int:category_id>/notes/<int:note_id>/<is_success>/delete', views.deleteNote, name='deleteNote'),
     path('quillText/', views.quillText, name='quillText')
 ]
